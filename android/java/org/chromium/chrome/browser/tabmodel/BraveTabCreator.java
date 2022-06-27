9ffa17487474cb80688cd0c31eb4fd2221b83e1d
Rakotomalala Mamy <mamy.else@gmail.com>
2022-06-27 12:37:11 +0300

    Set homepage default to presearch.com

--- android/java/org/chromium/chrome/browser/tabmodel/BraveTabCreator.java
+++ android/java/org/chromium/chrome/browser/tabmodel/BraveTabCreator.java
@@ -45,7 +45,7 @@ public class BraveTabCreator extends ChromeTabCreator {
     public Tab launchUrl(String url, @TabLaunchType int type) {
         if (url.equals(UrlConstants.NTP_URL)
                 && (type == TabLaunchType.FROM_CHROME_UI || type == TabLaunchType.FROM_STARTUP)) {
-            if (HomepageManager.getInstance().getHomepageUri() == null) {
+            if (HomepageManager.getHomepageUri() == null) {
                 url = "https://presearch.com";
             }
             ChromeTabbedActivity chromeTabbedActivity = BraveActivity.getChromeTabbedActivity();
@@ -71,7 +71,7 @@ public class BraveTabCreator extends ChromeTabCreator {
     public Tab createNewTab(LoadUrlParams loadUrlParams, @TabLaunchType int type, Tab parent) {
         if (loadUrlParams.getUrl().equals(UrlConstants.NTP_URL)
                 && type == TabLaunchType.FROM_TAB_GROUP_UI) {
-            if (HomepageManager.getInstance().getHomepageUri() == null) {
+            if (HomepageManager.getHomepageUri() == null) {
                 loadUrlParams.setUrl("https://presearch.com");
             }
         }
