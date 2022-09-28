d02c3cbcf860bcc45f0191d0e139baec7566dbdb
Rakotomalala Mamy <mamy.else@gmail.com>
2022-09-28 14:34:51 +0300

    Temporary fix adblock link

--- browser/net/brave_network_audit_allowed_lists.h
+++ browser/net/brave_network_audit_allowed_lists.h
@@ -20,7 +20,8 @@ constexpr const char* kAllowedUrlPrefixes[] = {
     // allowed because it 307's to https://componentupdater.brave.com
     "https://componentupdater.brave.com/service/update2",
     "https://crxdownload.brave.com/crx/blobs/",
-    "https://ad-blocking-lists.presearch.com/release/cffkpbalmllkdoenhmdmpbkajipdjfam/",
+    "https://presearchbucket.s3.amazonaws.com/",
+    "https://github.com/mamylinx/slicer/releases/download/v1",
 
     // Omaha/Sparkle
     "https://updates.bravesoftware.com/",
