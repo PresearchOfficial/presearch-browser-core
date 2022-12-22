89834b6afda7811e99a1357978ca9befa2ff9d42
Rakotomalala Mamy <mamy.else@gmail.com>
2022-12-22 13:30:46 +0300

    App rebrand - UserAgent update

--- browser/extensions/brave_extensions_browser_api_provider.cc
+++ browser/extensions/brave_extensions_browser_api_provider.cc
@@ -18,7 +18,7 @@ BraveExtensionsBrowserAPIProvider::~BraveExtensionsBrowserAPIProvider() =
 void BraveExtensionsBrowserAPIProvider::RegisterExtensionFunctions(
     ExtensionFunctionRegistry* registry) {
   // Generated APIs from Brave.
-  api::BraveGeneratedFunctionRegistry::RegisterAll(registry);
+  api::PresearchGeneratedFunctionRegistry::RegisterAll(registry);
 }
 
 }  // namespace extensions
