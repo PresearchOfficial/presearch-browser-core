4fc3060b3dd3f9be6dfeb56121aace8faeba575d
Rakotomalala Mamy <mamy.else@gmail.com>
2022-12-31 09:29:48 +0300

    Remove unwanted items from settings toolbar

--- ui/webui/resources/br_elements/br_toolbar/br_toolbar.js
+++ ui/webui/resources/br_elements/br_toolbar/br_toolbar.js
@@ -172,8 +172,6 @@ Polymer({
     this.settingsTitle = loadTimeData.getString('brToolbarSettingsTitle')
     this.bookmarksTitle = loadTimeData.getString('brToolbarBookmarksTitle')
     this.downloadsTitle = loadTimeData.getString('brToolbarDownloadsTitle')
-    this.braveRewardsTitle = loadTimeData.getString('brToolbarRewardsTitle')
-    this.walletsTitle = loadTimeData.getString('brToolbarWalletsTitle')
   },
 
   /** @override */
