3b4716d01c4c28939a8682f3973c479b2ed18d49
Rakotomalala Mamy <mamy.else@gmail.com>
2022-12-31 09:36:27 +0300

    Delete wallet and sync link - cleanup

--- browser/ui/webui/brave_settings_ui.cc
+++ browser/ui/webui/brave_settings_ui.cc
@@ -15,8 +15,6 @@
 #include "brave/browser/ui/webui/settings/brave_appearance_handler.h"
 #include "brave/browser/ui/webui/settings/brave_default_extensions_handler.h"
 #include "brave/browser/ui/webui/settings/brave_privacy_handler.h"
-#include "brave/browser/ui/webui/settings/brave_sync_handler.h"
-#include "brave/browser/ui/webui/settings/brave_wallet_handler.h"
 #include "brave/browser/ui/webui/settings/default_brave_shields_handler.h"
 #include "brave/components/brave_vpn/buildflags/buildflags.h"
 #include "brave/components/brave_wallet/common/features.h"
@@ -50,8 +48,6 @@ BraveSettingsUI::BraveSettingsUI(content::WebUI* web_ui,
   web_ui->AddMessageHandler(std::make_unique<DefaultBraveShieldsHandler>());
   web_ui->AddMessageHandler(std::make_unique<BraveDefaultExtensionsHandler>());
   web_ui->AddMessageHandler(std::make_unique<BraveAppearanceHandler>());
-  web_ui->AddMessageHandler(std::make_unique<BraveSyncHandler>());
-  web_ui->AddMessageHandler(std::make_unique<BraveWalletHandler>());
 }
 
 BraveSettingsUI::~BraveSettingsUI() {}
