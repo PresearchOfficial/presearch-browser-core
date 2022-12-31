e5092e8ce5916d18edcaaa27f4768ec2aedfb876
Rakotomalala Mamy <mamy.else@gmail.com>
2022-12-31 09:24:09 +0300

    Delete wallet and sync link

--- browser/ui/webui/ethereum_remote_client/ethereum_remote_client_page.tsx
+++ browser/ui/webui/ethereum_remote_client/ethereum_remote_client_page.tsx
@@ -3,13 +3,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
-import * as React from 'react'
-import { render } from 'react-dom'
-import { initLocale } from 'brave-ui'
-
-import 'emptykit.css'
-import '../../../../ui/webui/resources/fonts/poppins.css'
-import '../../../../ui/webui/resources/fonts/muli.css'
 
 function initialize () {
 }
