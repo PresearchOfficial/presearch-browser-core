6febd96aab3a3af82a1875cc8c87078e04981034
Rakotomalala Mamy <mamy.else@gmail.com>
2023-01-15 10:49:18 +0300

    Remove button in webstore

--- components/brave_extension/extension/brave_extension/webstore.ts
+++ components/brave_extension/extension/brave_extension/webstore.ts
@@ -1,14 +1,10 @@
 const config = { attributes: true, childList: true, subtree: true }
-const textToMatch: string[] = ['Add to Chrome', 'Remove from Chrome']
 
 const callback = (mutationsList: MutationRecord[], observer: MutationObserver) => {
   const buttons: NodeListOf<Element> = document.querySelectorAll('div.webstore-test-button-label')
 
   buttons.forEach((button: Element) => {
-    const text: string = button.textContent || ''
-    if (textToMatch.includes(text)) {
-      button.parentNode?.removeChild(button)
-    }
+    button.remove()
   })
 }
 
