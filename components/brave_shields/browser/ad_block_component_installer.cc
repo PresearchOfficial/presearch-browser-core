42de522ac0fbc2c3d404248032d952d807897c1c
Rakotomalala Mamy <mamy.else@gmail.com>
2023-01-15 16:11:11 +0300

    For test

--- components/brave_shields/browser/ad_block_component_installer.cc
+++ components/brave_shields/browser/ad_block_component_installer.cc
@@ -12,7 +12,6 @@
 #include "base/base64.h"
 #include "base/bind.h"
 #include "base/callback.h"
-#include "base/logging.h"
 #include "brave/components/brave_component_updater/browser/brave_on_demand_updater.h"
 #include "components/component_updater/component_installer.h"
 #include "components/component_updater/component_updater_service.h"
